c91284e4dd500934b4439d806863b873f533e0b9
Nahin Munkar <92095985+NahinM@users.noreply.github.com>
2025-01-23 18:26:45 +0600

    Update Structurepkg.h

--- DSAcpp/Algorithm/Structurepkg.h
+++ DSAcpp/Algorithm/Structurepkg.h
@@ -14,7 +14,7 @@ private:
 public:
     int nodes;
     vector<vector<pii>> graph;
-    Graph(int n,bool dir = true){
+    Graph(int n,bool dir = false){
         nodes = n;
         directed = dir;
         graph.resize(n);
@@ -48,4 +48,61 @@ void Graph::print(){
             printf(" -> %d(%d)",e.first,e.second);
         }cout << '\n';
     }
+}
+
+class Disjn_set
+{
+private:
+    int *arr = nullptr;
+    int size = 0;
+    int pathCompression(int a);
+    int components = 0;
+public:
+    Disjn_set(int sz){
+        arr = new int[sz];
+        for(int i=0;i<sz;i++) arr[i]=i;
+        components = size = sz;
+    }
+    int parent(int a);
+    int getSize();
+    int getComponentNum();
+    void frnd(int a, int b);
+    void print();
+    bool isFrnd(int a, int b);
+};
+
+int Disjn_set::getSize(){
+    return size;
+}
+
+int Disjn_set::getComponentNum(){
+    return components;
+}
+
+int Disjn_set::pathCompression(int a){
+    if(arr[a] == a) return a;
+    arr[a] = pathCompression(arr[a]);
+    return arr[a];
+}
+
+int Disjn_set::parent(int a){
+    if(a<0 || a>=size) return -1;
+    if(arr[a] == a) return a;
+    return pathCompression(a);
+}
+
+void Disjn_set::frnd(int a,int b){
+    if(a<0 || a>=size || b<0 || b>=size) return;
+    arr[parent(a)] = parent(b);
+    components--;
+}
+
+void Disjn_set::print(){
+    for(int i=0;i<size;i++) 
+        cout << i << '(' << arr[i] << ") ";
+    cout << '\n';
+}
+
+bool Disjn_set::isFrnd(int a, int b){
+    return parent(a)==parent(b);
 }
