fa7dee0bae5fa796bc50eb87bab46f1ce3ff3cd0
Nahin Munkar <92095985+NahinM@users.noreply.github.com>
2025-02-17 15:40:24 +0600

    Update ternarySearch.cpp

--- DSAcpp/Algorithm/Searching/ternarySearch.cpp
+++ DSAcpp/Algorithm/Searching/ternarySearch.cpp
@@ -4,19 +4,21 @@ using namespace std;
 
 int ternarySearch(const vector<int> &arr, int a){
     int l=0,h=arr.size(),mid1,mid2;
-    
+
+    // divide the array into three sections
     while(h-l>5){
         mid1 = l+(h-l)/3;
         mid2 = h-(h-l)/3;
 
-        if(a<arr[mid1]) h = mid1;
-        else if(arr[mid2]<a) l = mid2;
-        else {
+        if(a<arr[mid1]) h = mid1; // first part of three section
+        else if(arr[mid2]<a) l = mid2; // last part of three section 
+        else { // middle part of three section
             l = mid1;
             h = mid2;
         }
     }
 
+    // return the index of the element
     for(int i=l;i<=h;i++) if(arr[i]==a) 
         return i;
     return -1;
