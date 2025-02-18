664efa52dbf30f2b77dbc355846bbef87dd82121
Nahin Munkar <92095985+NahinM@users.noreply.github.com>
2025-02-18 20:18:35 +0600

    Update countSort.py

--- DSApython/Algorithm/Sorting/countSort.py
+++ DSApython/Algorithm/Sorting/countSort.py
@@ -1,59 +1,44 @@
-#include <iostream>
-#include <vector>
-using namespace std;
 
-void print(const vector<int> &arr){
-    for(int i:arr) cout << i << ' ';
-    cout << '\n';
-}
+def countSort1(arr):
+    MAX = MIN = arr[0]
+    for i in arr:
+        if i>MAX: MAX=i
+        if i<MIN: MIN=i
 
-void countSort1(vector<int> &arr){
-    int MAX=arr[0], MIN=arr[0], len = arr.size();
-
-    for(int i:arr){
-        if(i<MIN) MIN = i;
-        if(i>MAX) MAX = i;
-    }
-
-    vector<int> count(MAX-MIN+1,0);
-    for(int i:arr) count[i-MIN]++;
-
-    int at = 0,i=0;
-    while (at<len){
-        if(count[i]==0) i++;
-        else{
-            arr[at++] = i+MIN;
-            count[i]--;
-        }
-    }
+    count = [0]*(MAX-MIN+1)
+    for i in arr: count[i-MIN]+=1
     
-}
+    at,i = 0,0
+    ln = len(arr)
+    while at<ln:
+        if count[i]==0: i += 1
+        else:
+            arr[at]=i+MIN
+            count[i]-=1
+            at+=1
 
-vector<int> countSort2(const vector<int> &arr){
-    int MAX=arr[0], MIN=arr[0], len = arr.size();
 
-    for(int i:arr){
-        if(i>MAX) MAX=i;
-        if(i<MIN) MIN=i;
-    }
+def countSort2(arr):
+    MAX=MIN=arr[0]
+    for i in arr:
+        if i<MIN: MIN = i
+        if i>MAX: MAX = i
     
-    vector<int> count(MAX-MIN+1,0);
-    for(int i:arr) count[i-MIN]++;
-
-    for(int i=1; i<MAX-MIN+1; i++) count[i] += count[i-1];
+    count = [0]*(MAX-MIN+1)
+    for i in arr: count[i-MIN]+=1
     
-    vector<int> arr2(len);
-    for(int i=len-1; i>=0; i--){
-        int num = arr[i]-MIN;
-        arr2[--count[num]] = num+MIN;
-    }
-    return arr2;
-}
-
-int main(){
-    vector <int> arr = {10,16,19,13,17,12,11};
-    vector<int> arr2 = countSort2(arr);
-    print(arr2);
-    countSort1(arr);
-    print(arr);
-}
+    ln = len(arr)
+    for i in range(1,MAX-MIN+1): count[i]+=count[i-1]
+    
+    arr2 = [0]*ln
+    for i in range(ln-1,-1,-1):
+        x = arr[i]-MIN
+        count[x]-=1
+        arr2[count[x]] = x+MIN
+    
+    return arr2
+    
+arr = [10,16,19,13,17,12,11]
+print(countSort2(arr))
+countSort1(arr)
+print(arr)
