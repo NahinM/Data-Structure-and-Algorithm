f8bb0a554d25f85cda97e510bd566620846bd6fd
Nahin Munkar <92095985+NahinM@users.noreply.github.com>
2025-02-21 17:56:55 +0600

    Update binarySearch.py

--- DSApython/Algorithm/Searching/binarySearch.py
+++ DSApython/Algorithm/Searching/binarySearch.py
@@ -1,14 +1,15 @@
 
-def binarySearch2(arr,a):
+def binarySearch(arr,a):
     lw,hi = 0,len(arr)-1
 
     while lw<=hi:
         mid = (hi+lw)>>1
         if arr[mid] == a: return True
-        (lw := (mid+1)) if arr[mid]<a else (hi := (mid-1))
+        elif arr[mid] < a: lw = mid+1
+        else : hi = mid-1
     return False
 
 arr = [(i+1)*2 for i in range(20)]
 
 for i in range(1,40+1):
-    print(f"{i}\t: {"Yes" if binarySearch2(arr,i) else "NO"}")
+    print(f"{i}\t: {"Yes" if binarySearch(arr,i) else "NO"}")
